24ba95cb930bf796a6d48a839fdc0ba3814e3940
AndersonTucunduva <andersonpreuhs@gmail.com>
2024-08-13 15:51:13 -0300

    fix eslint

--- src/components/AnimatedTitle/AnimatedTitle.tsx
+++ src/components/AnimatedTitle/AnimatedTitle.tsx
@@ -6,15 +6,15 @@ const AnimatedTitle = () => {
     <div className="flex h-screen items-center justify-center bg-muted">
       <h1 className="text-6xl font-bold text-white">
         <span className="animate-fadeIn">D</span>
-        <span className="delay-[0.1s] animate-fadeIn">u</span>
-        <span className="delay-[0.2s] animate-fadeIn">t</span>
-        <span className="delay-[0.3s] animate-fadeIn">y</span>
-        <span className="delay-[0.4s] animate-fadeIn"> </span>
-        <span className="delay-[0.5s] animate-fadeIn">M</span>
-        <span className="delay-[0.6s] animate-fadeIn">a</span>
-        <span className="delay-[0.7s] animate-fadeIn">k</span>
-        <span className="delay-[0.8s] animate-fadeIn">e</span>
-        <span className="delay-[0.9s] animate-fadeIn">r</span>
+        <span className="delay-&lsqb;0.1s&rsqb animate-fadeIn">u</span>
+        <span className="delay-&lsqb;0.2s&rsqb animate-fadeIn">t</span>
+        <span className="delay-&lsqb;0.3s&rsqb animate-fadeIn">y</span>
+        <span className="delay-&lsqb;0.4s&rsqb animate-fadeIn"> </span>
+        <span className="delay-&lsqb;0.5s&rsqb animate-fadeIn">M</span>
+        <span className="delay-&lsqb;0.6s&rsqb animate-fadeIn">a</span>
+        <span className="delay-&lsqb;0.7s&rsqb animate-fadeIn">k</span>
+        <span className="delay-&lsqb;0.8s&rsqb animate-fadeIn">e</span>
+        <span className="delay-&lsqb;0.9s&rsqb animate-fadeIn">r</span>
       </h1>
     </div>
   )
